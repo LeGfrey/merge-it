da778a7f6b69305845d0bbc0e1bf197686563ea1
legfrey <gfreycandeias@gmail.com>
2016-07-03 09:07:23 +0200

    Add user authentication

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}

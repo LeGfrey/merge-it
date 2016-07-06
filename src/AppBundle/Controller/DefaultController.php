ef9f0cf763eee2db8d1289222ec023a0bdd57f01
legfrey <gfreycandeias@gmail.com>
2016-07-06 15:28:31 +0200

    Add the a way to contact me

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,5 +18,27 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request) {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject($form->get('subject')->getData())
+                ->setFrom($form->get('email')->getData())
+                ->setTo('geoffrey@rssmerge.it')
+                ->setBody('FROM: '.$form->get('name')->getData().' ('.$request->getClientIp().')\n'.$form->get('message')->getData());
+            $this->get('mailer')->send($message);
+            $request->getSession()->getFlashBag()->add('success', 'Your email has been sent! Check your inbox for an answer.');
+        }
+
+        return $this->render('default/contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
